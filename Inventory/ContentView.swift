855674261023a9ab4b816956dd62aaedfca681dd
Sven Iffland <sven@aiffland.de>
2020-04-29 15:11:56 +0200

    Initial Commit

---
+++ Inventory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inventory
+//
+//  Created by Sven Iffland on 29.04.20.
+//  Copyright © 2020 Sven Iffland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
